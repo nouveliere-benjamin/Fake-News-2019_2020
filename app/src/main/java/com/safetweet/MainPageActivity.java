3e8278023e34b92d3689c8fe738fb0ba9cb3b62c
Percedal <moi.durand.72@gmail.com>
2020-02-18 17:50:10 +0100

    Chargement des tweets de la timeline Ajout Bouton de chargement de + de tweets Appuie sur le texte "ACCUEIL" scroll la timeline vers le haut

--- app/src/main/java/com/safetweet/MainPageActivity.java
+++ app/src/main/java/com/safetweet/MainPageActivity.java
@@ -1,45 +1,90 @@
 package com.safetweet;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
-import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.*;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.TweetView;
+import retrofit2.Call;
+
+import java.util.List;
 
 public class MainPageActivity extends AppCompatActivity {
-	Context ctx;
+	private LinearLayout layout = null;
+	//	private ListView listView = null;
+	private Button loadBtn = null;
+	private TextView acceuilText = null;
+	private ScrollView scrollView = null;
+	private String userName;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		ctx = this;
-		Twitter.initialize(ctx);
+		Twitter.initialize(this);
 		setContentView(R.layout.activity_main_page);
-		
+		layout = findViewById(R.id.tweetLayout);
+//		listView = findViewById(R.id.listView);
+		loadBtn = findViewById(R.id.loadMoreBtn);
+		loadBtn.setOnClickListener(v -> loadTweets());
+		scrollView = findViewById(R.id.scrollView);
+		acceuilText = findViewById(R.id.homeTextView);
+		acceuilText.setOnClickListener(v -> scrollView.fullScroll(View.FOCUS_UP));
+		userName = getIntent().getStringExtra("username");
 		loadTweets();
 	}
 	
 	private void loadTweets() {
-		LinearLayout layout = findViewById(R.id.tweetLayout);
-//		for (int i = 0; i < 15; i++) {
-//
-//			Tweet_old t = new Tweet_old("Toto" + i, "Je suis Toto " + i);
-//			View v = new TweetView(this, t);
-//			layout.addView(v);
-//			Log.d("TweetLayout", t.getUserName());
-//		}
+		Toast.makeText(MainPageActivity.this, "Chargement de plus de tweets", Toast.LENGTH_LONG).show();
+		int layoutNbChild = layout.getChildCount();
+		Long maxId = null;
+		if (layoutNbChild > 1) {
+			TweetView v = (TweetView) layout.getChildAt(layoutNbChild - 2);
+			Tweet t = v.getTweet();
+			maxId = t.getId();
+		}
+		TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
+		Call<List<Tweet>> show = twitterApiClient.getStatusesService().homeTimeline(10, null, maxId, null, null, null, null);
+		show.enqueue(new Callback<List<Tweet>>() {
+			@Override
+			public void success(Result<List<Tweet>> result) {
+				Toast.makeText(MainPageActivity.this, "Load ok : " + result.data.size(), Toast.LENGTH_LONG).show();
+				//Retire le btn de chargement des tweets
+				layout.removeViewAt(layoutNbChild - 1);
+				for (Tweet tweet : result.data)
+					layout.addView(new TweetView(MainPageActivity.this, tweet));
+				layout.addView(loadBtn);
+			}
+			
+			@Override
+			public void failure(TwitterException exception) {
+				Toast.makeText(MainPageActivity.this, "Load pas ok", Toast.LENGTH_LONG).show();
+			}
+		});
 
-//		final long tweetId = 510908133917487104L;
-//		TweetUtils.loadTweet(tweetId, new Callback<Tweet>() {
+
+//		Affichage des tweets d'un compte donné (ici forbes)
+//		UserTimeline userTimeline = new UserTimeline.Builder().screenName("forbes").build();
+//		TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+//				.setTimeline(userTimeline)
+//				.build();
+//		listView.setAdapter(adapter);
+
+
+//      Affichage de tweets connaissant leurs ids
+//		List<Long> tweetIds = Arrays.asList(1229779625934454784L, 1229779874484752384L);
+//		TweetUtils.loadTweets(tweetIds, new Callback<List<Tweet>>() {
 //			@Override
-//			public void success(Result<Tweet> result) {
-//				layout.addView(new TweetView(ctx, result.data));
+//			public void success(Result<List<Tweet>> result) {
+//				for(Tweet tweet : result.data)
+//					layout.addView(new TweetView(MainPageActivity.this, tweet));
 //			}
 //
 //			@Override
 //			public void failure(TwitterException exception) {
-//				// Toast.makeText(...).show();
+//				Toast.makeText(MainPageActivity.this, "Echec du chargement des tweets !", Toast.LENGTH_LONG).show();
 //			}
 //		});
 	}
