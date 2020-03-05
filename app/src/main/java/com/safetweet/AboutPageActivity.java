59c05c86b5d0a9d68ea18f10a7fc259b0a7b406d
Percedal <moi.durand.72@gmail.com>
2020-03-05 19:08:48 +0100

    Chargement des tweets (nouveaux et anciens)

--- app/src/main/java/com/safetweet/AboutPageActivity.java
+++ app/src/main/java/com/safetweet/AboutPageActivity.java
@@ -1,6 +1,5 @@
 package com.safetweet;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/safetweet/MainPageActivity.java
+++ app/src/main/java/com/safetweet/MainPageActivity.java
@@ -1,6 +1,5 @@
 package com.safetweet;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
@@ -10,15 +9,18 @@ import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.tweetui.TweetView;
 import retrofit2.Call;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class MainPageActivity extends AppCompatActivity {
+	private int nbLoadedTweets = 0;
 	private LinearLayout layout = null;
-	//	private ListView listView = null;
+	private ProgressBar progressBar = null;
 	private Button loadBtn = null;
 	private TextView acceuilText = null;
 	private ScrollView scrollView = null;
 	private String userName;
+	private int maxTweetsLoad;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,48 +28,118 @@ public class MainPageActivity extends AppCompatActivity {
 		Twitter.initialize(this);
 		setContentView(R.layout.activity_main_page);
 		layout = findViewById(R.id.tweetLayout);
-//		listView = findViewById(R.id.listView);
+		progressBar = findViewById(R.id.progressBar);
 		loadBtn = findViewById(R.id.loadMoreBtn);
-		loadBtn.setOnClickListener(v -> loadTweets());
+		loadBtn.setVisibility(View.INVISIBLE);
+		loadBtn.setOnClickListener(v -> {
+			loadBtn.setVisibility(View.INVISIBLE);
+			Toast.makeText(MainPageActivity.this, getResources().getText(R.string.tweets_loading_message), Toast.LENGTH_LONG).show();
+			loadTweets();
+		});
 		scrollView = findViewById(R.id.scrollView);
 		acceuilText = findViewById(R.id.homeTextView);
-		acceuilText.setOnClickListener(v -> scrollView.fullScroll(View.FOCUS_UP));
+		acceuilText.setOnClickListener(v -> {
+			scrollView.fullScroll(View.FOCUS_UP);
+			progressBar.setVisibility(View.VISIBLE);
+			Toast.makeText(MainPageActivity.this, getResources().getText(R.string.new_tweets_loading_message), Toast.LENGTH_LONG).show();
+			loadNewTweets();
+		});
 		userName = getIntent().getStringExtra("username");
+		maxTweetsLoad = getResources().getInteger(R.integer.max_tweets_load);
 		loadTweets();
 	}
 	
-	private void loadTweets() {
-		Toast.makeText(MainPageActivity.this, "Chargement de plus de tweets", Toast.LENGTH_LONG).show();
+	/**
+	 * Charge le tweets récents n'ayant pas encore étaient chargé.
+	 * C'est-à-dire, charge les tweets posté entre l'instant présent
+	 * et le tweet le plus récent deja chargé (celui qui apparait en premier dans la timeline.
+	 * Les nouveaux tweets sont ajouter au debut de la timeline.
+	 * Si aucun tweet n'est a charger, informe l'utilisateur via un toast.
+	 */
+	private void loadNewTweets() {
 		int layoutNbChild = layout.getChildCount();
-		Long maxId = null;
-		if (layoutNbChild > 1) {
-			TweetView v = (TweetView) layout.getChildAt(layoutNbChild - 2);
-			Tweet t = v.getTweet();
-			maxId = t.getId();
+		if(nbLoadedTweets == 0) {
+			return;
 		}
+		Tweet t = ((TweetView) layout.getChildAt(1)).getTweet();
 		TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
-		Call<List<Tweet>> show = twitterApiClient.getStatusesService().homeTimeline(10, null, maxId, null, null, null, null);
+		Call<List<Tweet>> show = twitterApiClient.getStatusesService().homeTimeline(null, t.getId(), null, null, null, null, null);
 		show.enqueue(new Callback<List<Tweet>>() {
 			@Override
 			public void success(Result<List<Tweet>> result) {
-				Toast.makeText(MainPageActivity.this, "Load ok : " + result.data.size(), Toast.LENGTH_LONG).show();
-				//Retire le btn de chargement des tweets
-				layout.removeViewAt(layoutNbChild - 1);
-				for (Tweet tweet : result.data)
-					layout.addView(new TweetView(MainPageActivity.this, tweet));
-				layout.addView(loadBtn);
+				List<Tweet> tweets = new LinkedList(result.data);
+				//message si aucun nouveaux tweets a afficher
+				if(tweets.size() == 0) {
+					Toast.makeText(MainPageActivity.this, getResources().getText(R.string.no_new_tweets_loading_message), Toast.LENGTH_LONG).show();
+					return;
+				}
+				//affiche les nouveaux tweets
+				int idx = 0;
+				for (Tweet tweet : tweets) {
+					TweetView tv = new TweetView(MainPageActivity.this, tweet);
+					layout.addView(tv, idx++);
+					nbLoadedTweets++;
+				}
+				updateView();
 			}
 			
 			@Override
 			public void failure(TwitterException exception) {
-				Toast.makeText(MainPageActivity.this, "Load pas ok", Toast.LENGTH_LONG).show();
+				Toast.makeText(MainPageActivity.this, getResources().getText(R.string.tweets_loading_error_message), Toast.LENGTH_LONG).show();
+				updateView();
+			}
+			
+			private void updateView() {
+				progressBar.setVisibility(View.GONE);
 			}
 		});
 	}
 	
-	@Override
-	public void onBackPressed() {
-		Intent newActivity = new Intent(this, LoginPageActivity.class);
-		startActivity(newActivity);
+	/**
+	 * Charge de nouveaux tweets.
+	 * C'est-à-dire, charge des tweets plus ancien que le dernier (plus ancien) tweet deja chargé.
+	 * Le nombre de tweet a chargé est fixé par une constante (maxTweetsLoad).
+	 * Les tweets sont affichés à la suite des précédents.
+	 */
+	private void loadTweets() {
+		int layoutNbChild = layout.getChildCount();
+		//contiendra l'id du dernier tweet chargé (plus ancien)
+		Long maxId = null;
+		if (nbLoadedTweets != 0) {
+			//recupere le dernier tweet chargé
+			//attention -> le dernier element du layout est le bouton, le premier la progressbar
+			Tweet t = ((TweetView) layout.getChildAt(layoutNbChild - 2)).getTweet();
+			maxId = t.getId();
+		}
+		TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
+		//charge un tweet en plus du nombre fixé car le requete retourne aussi le tweet maxId
+		Call<List<Tweet>> show = twitterApiClient.getStatusesService().homeTimeline(maxTweetsLoad+1, null, maxId, null, null, null, null);
+		show.enqueue(new Callback<List<Tweet>>() {
+			@Override
+			public void success(Result<List<Tweet>> result) {
+				List<Tweet> tweets = new LinkedList(result.data);
+				//retire le premier tweet de la liste, apparaitra en double sinon
+				tweets.remove(0);
+				//affiche les tweets
+				int idx = layoutNbChild-1;
+				for (Tweet tweet : tweets) {
+					TweetView tv = new TweetView(MainPageActivity.this, tweet);
+					layout.addView(tv, idx++);
+					nbLoadedTweets++;
+				}
+				updateView();
+			}
+			
+			@Override
+			public void failure(TwitterException exception) {
+				Toast.makeText(MainPageActivity.this, getResources().getText(R.string.tweets_loading_error_message), Toast.LENGTH_LONG).show();
+				updateView();
+			}
+			
+			private void updateView() {
+				loadBtn.setVisibility(View.VISIBLE);
+				progressBar.setVisibility(View.GONE);
+			}
+		});
 	}
 }
